7c40b5a093700519d1303082a731c993079d11ac
Ahmed waleed <ahmed_waleed1@hotmail.com>
2021-10-25 21:46:41 +0500

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -17,3 +17,11 @@ func main() {
 
 	entry.Print()
 }
+// Example Output
+
+// minute          0 15 30 45
+// hour            0
+// day of month    1 15
+// month           1 2 3 4 5 6 7 8 9 10 11 12
+// day of week     1 2 3 4 5
+// command         /usr/bin/find
